c7771fbaa34d8fa75ea953504506e2788220271a
Philip Putnam <putnamp989@gmail.com>
2017-06-14 19:37:39 -0700

    installed body-parser for json data, created jsonCheck to see before & after parse, commented w/ old app.use for examples

--- app.js
+++ app.js
@@ -2,11 +2,40 @@
 
 var express = require('express');
 const app = express();
+var jsonParser = require('body-parser').json;
 
-app.use( (req, res, next) => {
-	console.log("The leave on the trees are", req.query.color);
+let jsonCheck = function(req, res, next) {
+	if(req.body) {
+		console.log('The sky is', req.body.color);
+	} else {
+		console.log('There is no body property on the request');
+	}
 	next();
-});
+}
+
+app.use(jsonCheck);
+app.use(jsonParser());
+app.use(jsonCheck);
+
+// Example of sending something to another api
+// app.use( (req, res, next) => {
+// 	req.myMessage = "hello, middleware #2";
+// 	next();
+// });
+// app.use( (req, res, next) => {
+// 	console.log(req.myMessage);
+// 	next();
+// });
+
+// example of order & getting the id from the url
+// app.use( (req, res, next) => {
+// 	console.log('First piece of middleware');
+// 	next();
+// });
+// app.use('/different/:id', (req, res, next) => {
+// 	console.log('Second piece of middleware', req.params.id);
+// 	next();
+// });
 
 const port = process.env.PORT || 3000;
 

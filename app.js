3beb7e00b005ea5cd80b6c0c80d5c5b8f9759671
Philip Putnam <putnamp989@gmail.com>
2017-06-14 19:03:24 -0700

    add middleware 'use' function, use postman to check to see if api gets are working

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict';
 
 var express = require('express');
-
 const app = express();
+
+app.use( (req, res, next) => {
+	console.log('First piece of middleware');
+	next();
+});
+
+app.use( (req, res, next) => {
+	console.log('Second piece of middleware');
+	next();
+});
 
 const port = process.env.PORT || 3000;
 

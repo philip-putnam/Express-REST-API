7c88dd4df1bcae3d19bb7cb2ae45b10b5b880080
Philip Putnam <putnamp989@gmail.com>
2017-06-14 18:55:24 -0700

    require express, call express(), set up port, console log after listening to port

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+'use strict';
 
+var express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log('node app.js worked!', port);
+});

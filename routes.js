3ff524fe46589824d642fa67a213ffc66f924415
Philip Putnam <putnamp989@gmail.com>
2017-06-14 20:32:48 -0700

    add validation for up-vote/down-vote w/ a 404 error thrown for invalid request

--- routes.js
+++ routes.js
@@ -6,7 +6,6 @@ var router = express.Router();
 //GET /questions
 // Route for questions collection
 router.get('/', (req, res) => {
-	// var o = obj.prop;
 	res.json({response: 'You sent me a GET request'});
 });
 
@@ -62,7 +61,15 @@ router.delete('/:qID/answers/:aID', (req, res) => {
 //POST /questions/:qID/answers/:aID/vote-up
 //POST /questions/:qID/answers/:aID/vote-down
 // POST on a specific answer
-router.post('/:qID/answers/:aID/vote-:dir', (req, res) => {
+router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
+	if (req.params.dir.search(/^(up|down)$/) === -1) {
+		let err = new Error("not found");
+		err.status = 404;
+		next(err);
+	} else {
+		next();
+	}
+}, (req, res) => {
 	res.json({
 		response: "You sent me a POST request to /vote-" + req.params.dir,
 		questionId: req.params.qID,
